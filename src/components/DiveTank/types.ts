97a5429bcff796f970c4421a271886bb8651a892
Clement Benezech <clement.b@obat.fr>
2024-01-12 14:51:44 +0700

    rebuilding

--- src/components/DiveTank/types.ts
+++
@@ -1,9 +0,0 @@
-export type GasProps = {
-  percentage: number;
-  color: string;
-  name: string;
-};
-
-export type DiveTankProps = {
-  gases: GasProps[];
-};
--- src/components/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const minSafePPO2 = 0.16;
-export const maxSafePPO2 = 1.4;
-
-export const oxygenColor = "#0aad9e";
-export const nitrogenColor = "#00a0db";
-
-export const dangerColor = "#ff6e6e";
-export const safeColor = "#00c368";
